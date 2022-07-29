6e19d86f9c360fdf78db514a93942c87d28792d9
sanderscody26 <sanderscody26@gmail.com>
2022-07-28 20:40:50 -0400

    Set up state, useEffect, and data fetch for feed

--- scrimbles_frontend/src/components/Feed.jsx
+++ scrimbles_frontend/src/components/Feed.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { client } from '../client'
+import MasontryLayout from './MasonryLayout.jsx'
+// import Spinner from './Spinner'
+import { Skeleton } from 'react-loading-skeleton'
+import { feedQuery, searchQuery } from '../utils/data'
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+  const { categoryId } = useParams()
+  const [scrimbl, setScrimbl] = useState(null)
+
+  useEffect(() => {
+    if (categoryId) {
+      const query = searchQuery(categoryId)
+      client.fetch(query).then((data) => {
+        setScrimbl(data)
+      })
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setScrimbl(data)
+      })
+    }
+  }, [categoryId])
+
+  return <div>Feed</div>
 }
 
 export default Feed
--- scrimbles_frontend/src/utils/data.js
+++ scrimbles_frontend/src/utils/data.js
@@ -1,5 +1,54 @@
 export const userQuery = (userId) => {
-    const query = `*[_type == "user" && _id == '${userId}']`
+  const query = `*[_type == "user" && _id == '${userId}']`
 
-    return query
+  return query
 }
+
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == 'scrimbl' && title match '${searchTerm}*' || category match '${searchTerm}' || about match ]{
+        image {
+            asset -> {
+                url
+            }
+        },
+        _id,
+        destination,
+        postedBy => {
+            _id,
+            userName,
+            image
+        },
+        save[] {
+          _key,
+          postedBy -> {
+            _id,
+            userName,
+            image
+          },
+        },
+    }`
+  return query
+}
+
+export const feedQuery = `*[_type == 'scrimbl'] | order(_createAt desc)] {
+  image {
+            asset -> {
+                url
+            }
+        },
+        _id,
+        destination,
+        postedBy => {
+            _id,
+            userName,
+            image
+        },
+        save[] {
+          _key,
+          postedBy -> {
+            _id,
+            userName,
+            image
+          },
+        },
+    }`
